500a7609c1bdd8a42d88e9b9633aa2435dc50a81
pasan <psa@codehousegroup.com>
2014-09-19 15:14:01 +0530

    Add Develop button.

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -21,5 +21,10 @@ namespace WindowsFormsApplication1
         {
             MessageBox.Show("Pasna");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Develp");
+        }
     }
 }
